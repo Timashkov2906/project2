a45b288047417ae243427cde1d43587868cc78fc
Pavel <pav.timashkov@gmail.com>
2022-03-13 18:15:01 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+ 
+/*console.log("Добро пожаловать");
+
+const obj={
+    name: "John",
+    age:26,
+    isMarried:false
+};
+let arr=["photo.jpg", "advanture.png,",{a:10, b:4},"picture.bmp" ];
+console.log(arr[2]);
+obj.name="Kail";
+console.log(obj.name);
+*/
+//const ansver=prompt("Вы тут!");
+//const ansver=confirm("Вы тут!");
+//const ansver=+prompt("Вам есть 18 лет","18");
+//console.log(ansver+5);
+const answers=[];
+answers[0]=prompt("Как Ваше имя ", "");
+answers[1]=prompt("Как Ваша фамилия ", "");
+answers[2]=prompt("Сколько вам лет ", "");
+document.write(answers);
